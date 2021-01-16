509c23bcfbaeaf486bf0c2e0b078235c27577945
Sitompul, Julio <julio.sitompul@aia.com>
2021-01-16 13:28:17 +0700

    Fix minor for models

--- api/models/users.js
+++ api/models/users.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  username: { type: String, unique: true },
+  username: { type: String, required: true },
   name: { type: String, required: true },
   email: {
     type: String,
